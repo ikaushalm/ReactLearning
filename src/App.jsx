98a26221686060903544e513c4a6952ed37f9160
KaushalPatel <kaushal.patel@zigram.tech>
2025-04-11 20:00:22 +0530

    Initiation of react learning

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import KgButton from "./KgButton";
+
+function App(){
+  return (
+    <div>
+      <h1>My React App</h1>
+      <p>This is a simple React application.</p>
+      <KgButton />
+      <p>Click the button above to like this video.</p>
+    </div>
+  );
+}
+export default App;
---
+++ src/Random.jsx
@@ -0,0 +1,8 @@
+function Random() {
+  return (
+    <div>
+      <h1>Random</h1>
+      <p>This is the random page.</p>
+    </div>
+  );
+}
